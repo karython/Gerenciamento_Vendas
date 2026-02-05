4d7a57f0345d6e1a617b50ce20d511f67b0d7a98
karython <karython.unai@gmail.com>
2026-02-05 15:01:47 -0300

    atualizacao lib para deploy

--- sistema_vendas/app_controle/apps.py
+++ sistema_vendas/app_controle/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class ControleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_controle'
+    name = 'sistema_vendas.app_controle'

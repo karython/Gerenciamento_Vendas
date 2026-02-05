cb5f66c711f2f7a39f67fd8792d5c4d4bc7b590f
karython <karython.unai@gmail.com>
2026-02-05 14:55:02 -0300

    atualizacao lib para deploy

--- sistema_vendas/manage.py
+++ sistema_vendas/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_vendas.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sistema_vendas.sistema_vendas.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

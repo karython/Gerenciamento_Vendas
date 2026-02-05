4dafbca385c30afdd96cb7b0d27aee9553c99e36
karython <karython.unai@gmail.com>
2026-02-05 14:58:20 -0300

    atualizacao lib para deploy

--- sistema_vendas/sistema_vendas/settings.py
+++ sistema_vendas/sistema_vendas/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_controle'
+    'sistema_vendas.app_controle'
 ]
 
 MIDDLEWARE = [
